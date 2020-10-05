a1c77193d1927e60bf1054f91edb386f85273065
hanumdwi <dwihanum55@gmail.com>
2020-10-05 15:57:27 +0700

    trigger

--- resources/views/cetakbarcode.blade.php
+++ resources/views/cetakbarcode.blade.php
@@ -9,11 +9,26 @@
 <div class="coba" style="height: 68.031496px; width:143.622047px;"> 
         <img src="data:image/png;base64,{{DNS1D::getBarcodePNG('$barang_id', 'C128')}}" style="height: 37.795276px; width:143.622047px" alt="barcode" />
             
-            <div class="coba1" style="margin-top:-2px; margin-left:37.795276px;">
+            <div class="coba1" style="margin-top:-1px; margin-left:37.795276px;">
             <font size="2"><strong>{{$barang_id}}</strong></font>
             </div>
 
-            <div class="coba2" style="margin-top:-2px; margin-left:45.354331px;">
+            <div class="coba2" style="margin-top:-1px; margin-left:45.354331px;">
+                @foreach($barang as $b)
+                <font size="2"> {{$b->NAMA_BARANG}} </font>
+                @endforeach
+            </div>
+       </div>
+       </div>
+       <div></div>
+<div class="coba" style="height: 68.031496px; width:143.622047px;"> 
+        <img src="data:image/png;base64,{{DNS1D::getBarcodePNG('$barang_id', 'C128')}}" style="height: 37.795276px; width:143.622047px" alt="barcode" />
+            
+            <div class="coba1" style="margin-top:-1px; margin-left:37.795276px;">
+            <font size="2"><strong>{{$barang_id}}</strong></font>
+            </div>
+
+            <div class="coba2" style="margin-top:-1px; margin-left:45.354331px;">
                 @foreach($barang as $b)
                 <font size="2"> {{$b->NAMA_BARANG}} </font>
                 @endforeach
